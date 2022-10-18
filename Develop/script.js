481d831688b66862226d72c4d920fd088a88c3f3
Chito <Javier.martinez632@gmail.com>
2022-10-18 11:06:30 -0700

    fixed the localStorage and event listener

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,7 @@
 var dateDisplayEl = $('#date-display');
-var saveButton = $('.saveBtn');
-var descriptEl = $('.description').val();
 var containerEl = $('container')
 
-var hourEl = saveButton.parent('div').attr("id");
+
 
 //displays the current date
 function showDate () {
@@ -11,45 +9,45 @@ function showDate () {
     dateDisplayEl.text(currentD);
 }
 showDate();
-//edit this 
-    //adds eventlistener on saveBtn
-function saveText(){
-     localStorage.setItem(hourEl, descriptEl);
-};
-
-console.log(descriptEl);
-console.log(hourEl);
-console.log(hourEl);
-console.log(hourEl);
-console.log(hourEl);
-saveButton.on('click', saveText)
-
-    
-function scheduleTrack() {
-    var presentTime = moment().hour();
-
-    $('.time-block').each(function() {
-        //fix this
-        var timeSlot = $('.hour').val();
-    
-        if(timeSlot === presentTime) {
-            $('.time-block').addClass("present");
-            $('.time-block').removeClass("past");
-            $('.time-block').removeClass("future");
-
-        } else if(timeSlot < presentTime) {
-            $('.time-block').addClass("past");
-            $('.time-block').removeClass("present");
-            $('.time-block').removeClass("future");
-        } else {
-            $('.time-block').addClass("future");
-            $('.time-block').removeClass("past");
-            $('.time-block').removeClass("present");        
-        }
-        console.log(timeSlot);
-    });
  
-    };
+    //adds eventlistener on saveBtn
+$('.saveBtn').on('click', function () {
+    var descriptEl = $(this).siblings('.description').val();
+    var hourEl = $(this).parent().attr('id');
+
+    localStorage.setItem(hourEl, descriptEl);
+});
+
+
+
+
+
+    
+//function scheduleTrack() {
+  //  var presentTime = moment().hour();
+
+    //$('.time-block').each(function() {
+        //fix this
+      //  var timeSlot = $('.hour').val();
+    
+        //if(timeSlot === presentTime) {
+          //  $('.time-block').addClass("present");
+            //$('.time-block').removeClass("past");
+            //$('.time-block').removeClass("future");
+
+       // } else if(timeSlot < presentTime) {
+           // $('.time-block').addClass("past");
+         //   $('.time-block').removeClass("present");
+            //$('.time-block').removeClass("future");
+       // } else {
+         //   $('.time-block').addClass("future");
+           // $('.time-block').removeClass("past");
+            //$('.time-block').removeClass("present");        
+       // }
+        //console.log(timeSlot);
+    //});
+ 
+  //  };
     
     $("#nine .description").val(localStorage.getItem("nine"));
     $("#ten .description").val(localStorage.getItem("ten"));
@@ -61,4 +59,4 @@ function scheduleTrack() {
     $("#four .description").val(localStorage.getItem("four"));
     $("#five .description").val(localStorage.getItem("five"));
 
-    scheduleTrack();
+//    scheduleTrack();
